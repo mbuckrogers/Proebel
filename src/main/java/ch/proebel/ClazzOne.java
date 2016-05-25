c46b0eb944cef494be6c8d874658261dbe086628
thelf <none@nowhere.com>
2016-05-25 15:55:56 +0200

    added two variables or so

--- src/main/java/ch/proebel/ClazzOne.java
+++ src/main/java/ch/proebel/ClazzOne.java
@@ -3,7 +3,9 @@ package ch.proebel;
 public class ClazzOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 7;
+		int b = 10;
+		System.out.println("Hello");
 
 	}
 
