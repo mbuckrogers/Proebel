99f141e341421df3f872b26fa48a5a6a0e6b12cc
thelf <none@nowhere.com>
2016-05-25 17:15:18 +0200

    some git magic

---
+++ src/main/java/ch/proebel/ClazzOne.java
@@ -0,0 +1,20 @@
+package ch.proebel;
+
+public class ClazzOne {
+
+	public static void main(String[] args) {
+		// Provoke a conflict -> solved
+		int a = 5;
+		int b = 10;
+		System.out.println("Hello");
+	}
+	
+	private void gurke() {
+		int a = 0;
+		double d = 0.0;
+	}
+	private void ooops() {
+		int m = -1;
+	}
+	
+}
